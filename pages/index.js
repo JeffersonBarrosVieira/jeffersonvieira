c2dd3aa83818c7a10b239da2c09ff32efd6cf722
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-01-23 15:55:26 -0300

    home-page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@ function Home(){
     return (
         <div>
             <h1>O Jhonata é feio</h1><br/>
-            <li>e a Nega também</li>
+            <h1>e a Nega também</h1><br/>
+            <h1>já o Jefferson ;) é um lindão</h1><br/>
+            <h2>zapZap: 4002-8922</h2>
         </div>
         )
 }
