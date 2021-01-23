3d7839ad911e767dbb0988a241ee5e9a564ef2fa
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-01-23 13:20:17 -0300

    home-page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home(){
     return (
-        <h1>Teste</h1><br/>
-        <li>123</li>
+        <div>
+            <h1>Teste</h1><br/>
+            <li>123</li>
+        </div>
         )
 }
 
