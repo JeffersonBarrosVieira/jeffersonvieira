4f118f606a0e592b1d2607ab33cdd8fe9d9f00bf
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-01-23 19:00:56 -0300

    home-page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home(){
     return (
         <div>
-            <h1>Em construção...</h1><br/>
+            <h1>Em construção...</h1>
             <br/>
             <h1>(~ o _o)~</h1>
         </div>
