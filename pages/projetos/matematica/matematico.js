2d730cbc6d4eab9e6a64e7c11c42975f62931fcf
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-05-04 12:05:41 -0300

    pagina-projetos

--- pages/projetos/matematica/matematico.js
+++ pages/projetos/matematica/matematico.js
@@ -3,9 +3,10 @@ import Molde from '../_molde';
 
 const Matematico = (props) => {
     return(<>
-        <div id="molde" style={{backgroundImage: "linear-gradient(45deg, #06520a71, #120655a4 60%), url('/img/teste8.jpg')"}}>
-            <Molde titulo="Pojeto 1" imagem="/img/projetos/mecanico/braço-robotico.jpg">
-                Resumo do projeto
+        <div id="molde" style={{backgroundImage: "linear-gradient(45deg, #06520a71, #120655a4 60%), url('/img/teste8.jpg')",
+                                cursor: 'wait'}}>
+            <Molde titulo="Seção projetos" imagem="https://static.wixstatic.com/media/cc092a_bacf98feff054b139aa6430b1b73cf13~mv2.png/v1/fill/w_560,h_454,al_c,q_85,usm_0.66_1.00_0.01/site%20em%20constru%C3%A7%C3%A3o.webp">
+                Em construção...
             </Molde>
             {props.children}
         </div>
